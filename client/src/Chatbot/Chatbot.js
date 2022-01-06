96b6dc467a9a09d461e05ac25741ba8ca7783882
Leesungsup <tmdtjq0116@naver.com>
2022-01-06 18:52:39 +0900

    message

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -2,8 +2,10 @@ import React,{useEffect} from 'react'
 import Axios from 'axios'
 import {useDispatch,useSelector} from 'react-redux'
 import {saveMessage} from '../_actions/message_actions'
+import {List,Icon,Avatar} from 'antd'
 function Chatbot() {
     const dispatch=new useDispatch();
+    const messagesFromRedux=new useSelector(state=>state.message.messages);
     useEffect(() => {
         eventQuery('Welcome1')
         
@@ -24,8 +26,8 @@ function Chatbot() {
             text
         }
         try{
-        const response=await Axios.post('/api/dialogflow/textQeury',textQueryVariables)
-        const content= response.data.fulfillmentMessage[0]
+        const response=await Axios.post('/api/dialogflow/textQuery',textQueryVariables)
+        const content= response.data.fulfillmentMessages[0]
         conversation={
             who:'bot',
             content:content
@@ -51,8 +53,8 @@ function Chatbot() {
             event
         }
         try{
-        const response=await Axios.post('/api/dialogflow/eventQeury',eventQueryVariables)
-        const content= response.data.fulfillmentMessage[0]
+        const response=await Axios.post('/api/dialogflow/eventQuery',eventQueryVariables)
+        const content= response.data.fulfillmentMessages[0]
         let conversation={
             who:'bot',
             content:content
@@ -60,6 +62,7 @@ function Chatbot() {
         dispatch(saveMessage(conversation))
         console.log(conversation)
         }catch(error){
+            console.log(error)
             let conversation={
                 who:'bot',
                 content:{
@@ -81,13 +84,34 @@ function Chatbot() {
             e.target.value="";
         }
     }
+    const renderOneMessage=(message,i)=>{
+        console.log('message',message)
+        return <div>
+                <List.Item key={i} style={{ padding: '1rem' }}>
+                    <List.Item.Meta
+                        avatar={<Avatar icon />}
+                        title={message.who}
+                        description={message.content.text.text}
+                    />
+                </List.Item>
+            </div>
+    }
+    const renderMessage=(returnedMessages)=>{
+        if(returnedMessages){
+            return returnedMessages.map((message,i)=>{
+                return renderOneMessage(message,i)
+            })
+        }else{
+            return null
+        }
+    }
     return (
         <div style={{
             height: 700, width: 700,
             border: '3px solid black', borderRadius: '7px'
         }}>
             <div style={{ height: 644, width: '100%', overflow: 'auto' }}>
-
+                {renderMessage(messagesFromRedux)}
             </div>
             <input style={{
                     margin: 0, width: '100%', height: 50,
